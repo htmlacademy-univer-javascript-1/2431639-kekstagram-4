0342c9237f24b66a434baf610695e68a597114ee
Smorodina <ha.muhumajlkax@gmail.com>
2023-12-04 00:46:16 +0500

    module8-task2

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,15 +1,14 @@
-import { bigPictureElement, renderComments, commentCountElement, commentsLoaderElement } from './comments.js';
+import { bigPictureElement, setAllComments, commentCountElement, commentsLoaderElement, renderComments } from './comments.js';
 const bodyElement = document.querySelector('body');
 const cancelButtonElement = bigPictureElement.querySelector('.big-picture__cancel');
-
 const hideBigPicture = () => {
   bigPictureElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeyDown);
 };
 
-function onDocumentKeyDown(evt){
-  if(evt.key === 'Escape'){
+function onDocumentKeyDown(evt) {
+  if (evt.key === 'Escape') {
     evt.preventDefault();
     hideBigPicture();
   }
@@ -19,25 +18,30 @@ const onCancelButtonClick = () => {
   hideBigPicture();
 };
 
-const renderPictureDetails = ({url, likes, description}) =>{
+const renderPictureDetails = ({url, likes, description, comments}) => {
   bigPictureElement.querySelector('.big-picture__img img').src = url;
   bigPictureElement.querySelector('.big-picture__img img').alt = description;
   bigPictureElement.querySelector('.likes-count').textContent = likes;
   bigPictureElement.querySelector('.social__caption').textContent = description;
+
+  setAllComments(comments);
+};
+
+const loadMoreComments = () => {
+  renderComments();
 };
 
 const showBigPicture = (data) => {
   bigPictureElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
-  commentsLoaderElement.classList.add('hidden');
+  commentsLoaderElement.classList.remove('hidden');
   commentCountElement.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeyDown);
-
   renderPictureDetails(data);
-  renderComments(data.comments);
 
+  commentsLoaderElement.addEventListener('click', loadMoreComments);
 };
 
 cancelButtonElement.addEventListener('click', onCancelButtonClick);
 
-export{showBigPicture};
+export { showBigPicture };
