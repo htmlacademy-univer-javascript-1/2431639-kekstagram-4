02043cf82ac51fff1c1545dacdd3056a140eb185
Smorodina <ha.muhumajlkax@gmail.com>
2023-12-22 20:49:29 +0500

    module12-task1

---
+++ js/utils.js
@@ -0,0 +1,24 @@
+function getRandomNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function debounce(callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export{ debounce, throttle, getRandomNumber };
